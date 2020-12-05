1218e70c08fae245a4fb5af2b5d7a6aa400115e9
Meredith Jones <mj19003@gmail.com>
2020-12-05 16:51:14 -0500

    Saturday night updates

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// When the planner opens, the current date should display at the top
+
+
+//Set up a scroll feature?
+
+// hardcode "standard business hours in html"
+
+//create color coding to signify past, present and future dates. 
+
+// have to reference the current time to compare against the time blocks. Use jQuery methods to
+//add/remove element based on time. 
+
+// time blocks have to be editable with scheduled items for the given day. 
+
+
+// save entries for given days in local storage. 
+// save local storage for last once other functionality is in place!!
+
+
+// when the page is refreshed 
+// THEN the saved events persist and are retrieved from localStorage
