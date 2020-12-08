cc88ae448f3be4e7b5fdc30040d62f0a1d928797
Meredith Jones <mj19003@gmail.com>
2020-12-07 20:51:54 -0500

    Updates Monday night with color coding debug

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# day_planner
+Meredith Jones Day Planner 
+Created with Javascript
+
+In this project, I created a day planner designed for tracking tasks to be done during a user's work day. 
+
+When the page loads, the current date is displayed at the top of the planner. 
+
+For easy visual reference, the planner is color coded, so that once an event is in the past, the time block where it is recorded will turn grey- white as seen in the screenshot below, showing the planner viewed in the late evening with all events being in the past.
+![An image of the work day planner, with no entries and time blocks with a light grey bacground](assets/images/image1.png)
+
+Events in the current hour will have a red background, and future events will display in green blocks. 
+Below is a screenshot example of the planner if it were to be viewed at in the morning before 9am with some entries for the day displaying. 
+![An image of the work day planner,as it would appear in the morning before the work day starts. Some meetings and tasks are filled into the time blocks and they have a green background. ](assets/images/image2.png)
+
+Additionally, the planner will store inputs in local storage if a user presses the corresponding "Save" button located to the far right of each block of time. Inputs will be retained even if the page is refreshed, or the user leaves the page and returns to it. 
+
+For the date formating, I utilized dayjs and for the "save" icons on the time blocks, I used fontawesome. 
+
+This project definitely presented some complexity relating to looping through the times assigned to each block and comparing them to the present time, as well as in the process of storing and retrieving user inputs to local storage. Those two pieces of the project took the vast majority of my time. I am happy with the functionality and how much it looks like the given goal for appearance. 
--- script.js
+++ script.js
@@ -10,12 +10,13 @@ var currentDay = dayjs().format("dddd, MMMM D YYYY")
 // Displaying the current date at the top when the planner opens
 document.getElementById("currentDay").innerHTML = currentDay
 
+//Checking to make sure that dayjs is properly set up
 console.log(dayjs());
 
 //Setting up the on click method for save btn to target
 //user inputs for local storage.
 $(".saveBtn").on("click", function (){
-    console.log("WE WORK", $(this).parent().parent().attr("id"));
+    console.log("WE WORK",$(this).parent().parent().attr("id"));
     var value =  $(this).siblings("input").val();
     var time = $(this).parent().parent().attr("id");
 
@@ -37,32 +38,6 @@ $("#14 .input-group .form-control").val(localStorage.getItem("14"));
 $("#15 .input-group .form-control").val(localStorage.getItem("15"));
 $("#16 .input-group .form-control").val(localStorage.getItem("16"));
 $("#17 .input-group .form-control").val(localStorage.getItem("17"));
-
-
-//Connect time colums to day.js so we can color code our tasks columns according to
-//past, present, future
-
-//Create a for loop so the items in hoursArray get compared 
-//against the variable hoursDisplay
-// for (var i = 0; i < hoursArray.length; i++) {
-
-//   //Convert hoursDisplay string into a number and save
-//   // it in hoursDisplayType.
-
-//   var hoursDisplayType = parseInt(hoursDisplay);
-//   var AvailableHours = hoursArray[i];
-//   var ScheduleLoop = hoursDisplay[i];
-
-//   if (AvailableHours === hoursDisplayType) {
-//     $(ScheduleLoop).addClass("present");
-//   }
-//   if (AvailableHours < hoursDisplay) {
-//     $(ScheduleLoop).addClass("past");
-//   }
-//   if (AvailableHours > hoursDisplay) {
-//     $(ScheduleLoop).addClass("future");
-//   }
-// }
 
 // console.log(hoursDisplayType)
 
@@ -93,117 +68,11 @@ var input = $(this).children().children().children("input")
     input.removeClass("present")
      input.addClass("future");
    }
+
+   
      
 
     })
 };
 updateHour();
 setInterval(updateHour, 1500);
-
-
-
-
-
-
-
-//Setting up local storage
-//assigning variables to assign storage 
-
-// const storageContent = document.querySelector('.storage');
-// const text = document.querySelector('.form-control');
-// const button = document.querySelector('.bml-auto p-2 saveBtn')
-
-
-// button.addEventListener(onclick, task => {
-//     console.log(task.target.value)
-//     text.textContent.target.value
-
-// })
-
- // Put our ItemsToDo into an object 
-//  var ItemsToDo = {
-//     NineAM: textAreaEl[0].value.trim(),
-//     TenAM: textAreaEl[1].value.trim(),
-//     ElevenAM: textAreaEl[2].value.trim(),
-//     TwelvePM: textAreaEl[3].value.trim(),
-//     OnePM: textAreaEl[4].value.trim(),
-//     TwoPM: textAreaEl[5].value.trim(),
-//     ThreePM: textAreaEl[6].value.trim(),
-//     FourPM: textAreaEl[7].value.trim(),
-//     FivePM: textAreaEl[8].value.trim(),
-
-
-//Checking to see if local storage already exists
-// let items
-
-// if (localStorage.getItem('items')) {
-//   items = JSON.parse(localStorage.getItem('items'))
-// } else {
-//   items = []
-// }
-
-//When we reload the page, we need to see the most recent inputs from out to do list, re-displayed from local storage. 
-// var lastTasks = JSON.parse(localStorage.getItem("Todo"));
-
-//Need to create a "Todo" object when the save button is clicked. 
-
-//Need to store the "Todo" object in local storage. 
-// localStorage.setItem("Todo", JSON.stringify(Todo));
-
-
- //Creating the fucntion for the save button. When we click the button, 
- //the content for the corresponding time slot needs to save to local storage. 
-        // function saveToDo(event) {
-        //     event.preventDefault();
-
-//Calling the save function for our save button 
-// $(".saveBtn").on("click", saveToDo);
-
-//Write a function for each time slot in which if they are in the past, the section is blue-if it is in the present there
-//is no fill color, and if it is in the future, it is green.
-
-// const oneHour = document.getElementsByClassName("timeSlot");
-// console.log(oneHour);
-
-// let currentHour = parseInt(dayjs(dateToday).format('H'));
-// var timeSlotHour =
-
-// Array.from(oneHour).forEach(timeSlot => {
-//   let
-//     timeSlotIdString = timeSlot.id,
-//     timeSlotHour;
-//   if (timeSlotIdString) {
-//     timeSlotHour = parseInt(timeSlotIdString);
-//   }
-//   if (timeSlotHour) {
-//     // Compares row id to current hour and sets color accordingly
-//     if (currentHour === timeSlotHour) {
-//       setColor(timeSlot, "pink");
-//     } else if ((currentHour < timeSlotHour) && (currentHour > timeSlotHour - 6)) {
-//       setColor(timeSlot, "lightgray");
-//     } else if ((currentHour > timeSlotHour) && (currentHour < timeSlotHour + 6)) {
-//       setColor(timeSlot, "blue");
-//     } else {
-//       setColor(timeSlot, "white");
-//     }
-//   }
-// });
-
-// function setColor(element, color) {
-//   element.style.backgroundColor = color;
-// }
-
-// hardcode "standard business hours in html"
-
-//create color coding to signify past, present and future dates.
-
-// have to reference the current time to compare against the time blocks. Use jQuery methods to
-//add/remove element based on time.
-
-// time blocks have to be editable with scheduled items for the given day.
-
-// save entries for given days in local storage.
-// save local storage for last once other functionality is in place!!
-
-// when the page is refreshed
-// THEN the saved events persist and are retrieved from localStorage
